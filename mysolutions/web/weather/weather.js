41ce502e602b7900891c59161d79647cdf4f7b1d
AussieGuy0 <anthony.bruno196@gmail.com>
2017-01-08 16:37:45 +1030

    Add showing of celcius temp in user's location

--- mysolutions/web/weather/weather.js
+++ mysolutions/web/weather/weather.js
@@ -1,15 +1,17 @@
-var baseUrl = "api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}";
+var baseUrl = "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}";
 var apiKey = "01d7df07b3e64d8a355eaedec50045ad";
-var weatherElement = document.getElementById("weather");
+var weatherElement;
+var tempValue;
 
-navigator.geolocation.getCurrentPosition(handlePosition);
+document.addEventListener("DOMContentLoaded", function(event) {
+    weatherElement = document.getElementById("weather");
+    navigator.geolocation.getCurrentPosition(handlePosition);
+});
 
 function handlePosition(pos) {
     let url;
     var lat = pos.coords.latitude;
     var lon = pos.coords.longitude;
-    console.log(lat);
-    console.log(lon);
 
     url = baseUrl.replace("{lat}",lat);
     url = url.replace("{lon}",lon);
@@ -31,8 +33,25 @@ function makeApiRequest(url) {
 
 function handleApiReturn(responseText) {
     var json = JSON.parse(responseText);
-    var weather = json.weather(); 
-    var main = json.main();
-    weatherElement.innerHtml(main.temp());
-
+    var weather = json.weather;
+    var main = json.main;
+    tempValue = new Temp(main.temp);
+    weatherElement.innerHTML = tempValue.celciusTemp;
 }
+
+
+function Temp(kelvinTemp) {
+    this.kelvinTemp = kelvinTemp;
+    this.celciusTemp = kelvinToCelcius(kelvinTemp);
+    this.farenheightTemp = kelvinToFarenheight(kelvinTemp);
+}
+
+function kelvinToCelcius(kelvin) {
+    return kelvin - 273.15; 
+}
+
+function kelvinToFarenheight(kelvin) {
+    return kelvin * (9/5) - 459.67;
+}
+
+
