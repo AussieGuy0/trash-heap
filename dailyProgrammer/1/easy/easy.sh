149cddb6c79d2905657c1adc6bbb36d37cb3dda6
AussieGuy0 <anthony.bruno196@gmail.com>
2017-03-26 21:20:40 +1030

    Add first challenge from /r/dailyprogrammer

---
+++ dailyProgrammer/1/easy/easy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "What is your name?"
+read NAME
+echo "What is your age?"
+read AGE
+echo "What is your Reddit username?"
+read USER
+
+echo "your name is $NAME, you are $AGE years old, and your username is $USER"
