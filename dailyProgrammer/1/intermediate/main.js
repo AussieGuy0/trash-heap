fbde43a8b0fa5e0ab40e6708e3ac84ace728e792
AussieGuy0 <anthony.bruno196@gmail.com>
2017-04-02 13:35:00 +0930

    Clicking date now filters events by date

--- dailyProgrammer/1/intermediate/main.js
+++ dailyProgrammer/1/intermediate/main.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     nameInput = document.getElementById("name");
     dateInput = document.getElementById("date");
     dateHeader = document.getElementById("date-header");
-    dateHeader.addEventListener("click", () => sortTableByDate());
+    dateHeader.addEventListener("click", () => sortTableByDate(eventTable));
     addButton = document.getElementById("add-button");
     addButton.addEventListener("click", () => {
         addEvent(nameInput.value, dateInput.value, eventTable);
@@ -26,11 +26,39 @@ function addEvent(name, date, table) {
     dateCell.innerHTML = date;
 }
 
-function sortTableByDate() {
+function sortTableByDate(table) {
+    let rows = table.rows;
+    let workingRows = [];
+    for (let i = 1; i < rows.length; i++) { //push all rows to array
+        let row = rows[i];
+        workingRows.push(row);
+    }
+
+    let rowsLength = table.rows.length;
+    for (let i = 1; i < rowsLength; i++) { //delete all rows in table
+        table.deleteRow(1);
+    }
+
+    while (workingRows.length > 0) { //re-add rows in order of date
+        let indexOfMaxRow = 0;
+        let maxRow = workingRows[0];
+        for (let i = 1; i < workingRows.length; i++) {
+            let maxDate = maxRow.cells[1].innerHTML;
+            let currRow = workingRows[i];
+            let currDate = currRow.cells[1].innerHTML;
+            if (compareDates(maxDate, currDate) === -1) {
+               maxRow = currRow; 
+               indexOfMaxRow = i;
+            }
+        }
+        workingRows.splice(indexOfMaxRow, 1);
+        table.appendChild(maxRow);
+    }
+
 }
 
 function compareDates(first, second) {
-    let datePattern = /[0-9]{4}-[0-9]{2}-[0-9]{2}/g;
+    let datePattern = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
     if (!datePattern.test(first) || !datePattern.test(second)) {
         return;
     }
