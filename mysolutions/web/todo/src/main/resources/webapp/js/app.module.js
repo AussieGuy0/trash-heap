af5ccacaebfdb0ef993f7d4efc49fa8046c1a261
AussieGuy0 <anthony.bruno196@gmail.com>
2017-06-21 20:59:52 +0930

    Rewriting code to understand it better

--- mysolutions/web/todo/src/main/resources/webapp/js/app.module.js
+++ mysolutions/web/todo/src/main/resources/webapp/js/app.module.js
@@ -1,5 +1,16 @@
-var todoApp = angular.module('todoApp', [
-    'todoList',
-    'todoForm'
-]);
+var app = angular.module('todoApp', []);
+app.controller('TodoController', ['$http', '$window', function ($http, $window) {
+    let self = this;
+
+    $http.get('/api/todos').then(function (todos) {
+        self.todos = todos.data;
+    });
+
+    self.workingTodo = {};
+    this.submitTodo = () => {
+        $http.post('/api/todo', self.workingTodo);
+        self.workingTodo = {};
+        $window.location.reload();
+    };
+}]);
 
