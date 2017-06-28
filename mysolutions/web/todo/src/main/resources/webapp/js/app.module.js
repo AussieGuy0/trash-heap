6faf2f45552ceb1f608806bc7deb00f53416995d
AussieGuy0 <anthony.bruno196@gmail.com>
2017-06-28 19:27:55 +0930

    Add list sort functionality

--- mysolutions/web/todo/src/main/resources/webapp/js/app.module.js
+++ mysolutions/web/todo/src/main/resources/webapp/js/app.module.js
@@ -1,16 +1,19 @@
 var app = angular.module('todoApp', []);
-app.controller('TodoController', ['$http', '$window', function ($http, $window) {
+app.controller('TodoController', ['$http', function ($http) {
+    this.orderProp = "text";
     let self = this;
 
-    $http.get('/api/todos').then(function (todos) {
-        self.todos = todos.data;
-    });
+    this.loadData = function() {
+        $http.get('/api/todos').then(function (todos) {
+            self.todos = todos.data;
+        });
+    };
+    this.loadData();
 
     self.workingTodo = {};
     this.submitTodo = () => {
         $http.post('/api/todo', self.workingTodo);
         self.workingTodo = {};
-        $window.location.reload();
     };
 }]);
 
