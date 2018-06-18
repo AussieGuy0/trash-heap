ab82a1f8c2435ea1c2c4a72eaea8f06efc51f3ee
AussieGuy0 <anthony.bruno196@gmail.com>
2018-06-18 20:46:29 +0930

    Basic websocket support

--- mysolutions/web/tempChat/backend/index.js
+++ mysolutions/web/tempChat/backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const expressWs = require('express-ws')(app);
 
 app.get('/test', (req, res) => {
     res.send('abc');
+})
+
+app.ws('/room/:id', (ws, req) => {
+    ws.on('message', (msg) => {
+        ws.send(msg);
+    })
 })
 
 app.use(express.static('../frontend/dist/'))
